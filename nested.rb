eb61293009eca28dfc95b92d5c6a680536e32599
Oneil Williams <oneilswilliams@gmail.com>
2019-08-16 20:44:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-        :yukihiro_matsumoto
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP","C"]
+        }
      }
 
     
